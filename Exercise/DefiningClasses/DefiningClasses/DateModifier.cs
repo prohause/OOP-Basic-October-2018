2cd7dc256ecac4aa952d7dc3adaa029e00e8156e
Gyunay Daud <prohause@live.com>
2018-10-18 18:02:51 +0300

    Problem 05 solved

---
+++ Exercise/DefiningClasses/DefiningClasses/DateModifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    internal class DateModifier
+    {
+        private TimeSpan diff;
+
+        public void CalculateDiff(DateTime first, DateTime second)
+        {
+            diff = first - second;
+            Console.WriteLine(Math.Abs(diff.TotalDays));
+        }
+    }
+}
--- Exercise/DefiningClasses/DefiningClasses/Program.cs
+++ Exercise/DefiningClasses/DefiningClasses/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace DefiningClasses
 {
@@ -8,18 +6,10 @@ namespace DefiningClasses
     {
         public static void Main(string[] args)
         {
-            var count = int.Parse(Console.ReadLine());
-            var myFamily = new List<Person>();
-            for (int i = 0; i < count; i++)
-            {
-                var tokens = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                var name = tokens[0];
-                var age = int.Parse(tokens[1]);
-                var newMember = new Person(name, age);
-                myFamily.Add(newMember);
-            }
-
-            myFamily.Where(p => p.Age > 30).OrderBy(p => p.Name).ToList().ForEach(p => Console.WriteLine($"{p.Name} - {p.Age}"));
+            var first = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var second = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var myTest = new DateModifier();
+            myTest.CalculateDiff(DateTime.Parse(string.Join("/", first)), DateTime.Parse(string.Join("/", second)));
         }
     }
 }
