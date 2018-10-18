133b208866925fe2b52a5da33a0a6a2fa6a4833b
Gyunay Daud <prohause@live.com>
2018-10-18 07:19:14 +0300

    Problem 01 solved

---
+++ Exercise/DefiningClasses/DefiningClasses/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace DefiningClasses
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+    }
+}
