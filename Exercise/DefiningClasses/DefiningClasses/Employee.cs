5efedcea2a004e1f95573bdcb0051f8d02886ce7
Gyunay Daud <prohause@live.com>
2018-10-18 18:37:39 +0300

    Pronlem 06 solved

---
+++ Exercise/DefiningClasses/DefiningClasses/Employee.cs
@@ -0,0 +1,52 @@
+﻿namespace DefiningClasses
+{
+    internal class Employee
+    {
+        private readonly string _name;
+        private decimal _salary;
+        private string _position;
+        private string _department;
+        private readonly string _email;
+        private readonly int _age;
+
+        public Employee(string name, decimal salary, string position, string department, string email, int age)
+        {
+            _name = name;
+            SetSalary(salary);
+            _position = position;
+            _department = department;
+            _email = email;
+            _age = age;
+        }
+
+        public Employee(string name, decimal salary, string position, string department)
+            : this(name, salary, position, department, "n/a", -1)
+        {
+        }
+
+        public Employee(string name, decimal salary, string position, string department, int age)
+        : this(name, salary, position, department, "n/a", age)
+        {
+        }
+
+        public Employee(string name, decimal salary, string position, string department, string email)
+        : this(name, salary, position, department, email, -1)
+        {
+        }
+
+        public decimal GetSalary()
+        {
+            return _salary;
+        }
+
+        public void SetSalary(decimal value)
+        {
+            _salary = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{_name} {GetSalary():F2} {_email} {_age}";
+        }
+    }
+}
--- Exercise/DefiningClasses/DefiningClasses/Program.cs
+++ Exercise/DefiningClasses/DefiningClasses/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace DefiningClasses
 {
@@ -6,10 +8,47 @@ namespace DefiningClasses
     {
         public static void Main(string[] args)
         {
-            var first = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            var second = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            var myTest = new DateModifier();
-            myTest.CalculateDiff(DateTime.Parse(string.Join("/", first)), DateTime.Parse(string.Join("/", second)));
+            var count = int.Parse(Console.ReadLine());
+            var rooster = new Dictionary<string, List<Employee>>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var tokens = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                Employee current;
+                var department = tokens[3];
+                switch (tokens.Length)
+                {
+                    case 6:
+                        current = new Employee(tokens[0], decimal.Parse(tokens[1]), tokens[2], tokens[3], tokens[4], int.Parse(tokens[5]));
+                        break;
+
+                    case 5:
+                        try
+                        {
+                            current = new Employee(tokens[0], decimal.Parse(tokens[1]), tokens[2], tokens[3], int.Parse(tokens[4]));
+                        }
+                        catch (Exception)
+                        {
+                            current = new Employee(tokens[0], decimal.Parse(tokens[1]), tokens[2], tokens[3], tokens[4]);
+                        }
+
+                        break;
+
+                    default:
+                        current = new Employee(tokens[0], decimal.Parse(tokens[1]), tokens[2], tokens[3]);
+                        break;
+                }
+
+                if (!rooster.ContainsKey(department))
+                {
+                    rooster.Add(department, new List<Employee>());
+                }
+                rooster[department].Add(current);
+            }
+
+            var winner = rooster.OrderByDescending(p => p.Value.Average(x => x.GetSalary())).First();
+            Console.WriteLine($"Highest Average Salary: {winner.Key}");
+            winner.Value.OrderByDescending(p => p.GetSalary()).ToList().ForEach(Console.WriteLine);
         }
     }
 }
