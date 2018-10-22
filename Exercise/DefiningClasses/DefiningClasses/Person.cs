25ddc1242f6caf293546aa6206373ce863c7fd83
Gyunay Daud <prohause@live.com>
2018-10-22 20:47:56 +0300

    Problem 13 solved

--- Exercise/DefiningClasses/DefiningClasses/Person.cs
+++ Exercise/DefiningClasses/DefiningClasses/Person.cs
@@ -6,32 +6,16 @@ namespace DefiningClasses
     public class Person
     {
         private readonly string _name;
-        private string _dateOfBirth;
-        private Company _company;
-        private readonly List<Pokemon> _pokemons;
+        private readonly string _dateOfBirth;
         private readonly List<Person> _parents;
         private readonly List<Person> _children;
-        private Car _car;
 
-        public Person(string name)
+        public Person(string name, string birthday)
         {
             _name = name;
-            _dateOfBirth = null;
-            _company = null;
-            _pokemons = new List<Pokemon>();
+            _dateOfBirth = birthday;
             _parents = new List<Person>();
             _children = new List<Person>();
-            _car = null;
-        }
-
-        public void SetBirth(string input)
-        {
-            _dateOfBirth = input;
-        }
-
-        public void AddPokemon(Pokemon pokemon)
-        {
-            _pokemons.Add(pokemon);
         }
 
         public void AddParent(Person person)
@@ -39,19 +23,9 @@ namespace DefiningClasses
             _parents.Add(person);
         }
 
-        public void AddChildren(Person person)
+        public void AddChild(Person person)
         {
             _children.Add(person);
-        }
-
-        public void SetCar(Car car)
-        {
-            _car = car;
-        }
-
-        public void SetCompany(Company company)
-        {
-            _company = company;
         }
 
         public override string ToString()
@@ -64,13 +38,14 @@ namespace DefiningClasses
             return _name;
         }
 
+        public string GetBirth()
+        {
+            return _dateOfBirth;
+        }
+
         public void Print()
         {
-            Console.WriteLine($"{_name}");
-            Console.WriteLine($"Company:{_company}");
-            Console.WriteLine($"Car:{_car}");
-            Console.WriteLine("Pokemon:");
-            _pokemons.ForEach(Console.WriteLine);
+            Console.WriteLine(this);
             Console.WriteLine("Parents:");
             _parents.ForEach(Console.WriteLine);
             Console.WriteLine("Children:");
