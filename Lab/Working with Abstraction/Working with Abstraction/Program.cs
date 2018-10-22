26373a5599a7eaf550e774e5ea88f5a263772dbe
Gyunay Daud <prohause@live.com>
2018-10-22 21:03:45 +0300

    Initial commit

---
+++ Lab/Working with Abstraction/Working with Abstraction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Working_with_Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
