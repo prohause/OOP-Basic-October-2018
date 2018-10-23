a76a1b14926074a65c937a2d2a474a9411177da5
Gyunay Daud <prohause@live.com>
2018-10-23 14:59:45 +0300

    Problem 01 Solved

--- Lab/Working with Abstraction/Working with Abstraction/Program.cs
+++ Lab/Working with Abstraction/Working with Abstraction/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Linq;
 
 namespace Working_with_Abstraction
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var size = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= size; i++)
+            {
+                PrintRow(size, i);
+            }
+
+            for (int i = size - 1; i >= 1; i--)
+            {
+                PrintRow(size, i);
+            }
+        }
+
+        private static void PrintRow(int size, int i)
+        {
+            Console.WriteLine($"{new string(' ', size - i)}{string.Join(" ", Enumerable.Repeat("*", i))}");
         }
     }
 }
