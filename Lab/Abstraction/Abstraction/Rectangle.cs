020aa12b02f78121926ad42d80a5f376de5572a0
Gyunay Daud <prohause@live.com>
2018-10-23 15:29:53 +0300

    Problem 02 solved

---
+++ Lab/Abstraction/Abstraction/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿namespace Abstraction
+{
+    public class Rectangle
+    {
+        public Point TopLeftPoint
+        {
+            get; set;
+        }
+
+        public Point LowerRightPoint
+        {
+            get; set;
+        }
+
+        public Rectangle(Point topLeftPoint, Point lowerRightPoint)
+        {
+            TopLeftPoint = topLeftPoint;
+            LowerRightPoint = lowerRightPoint;
+        }
+
+        public bool Contains(Point point)
+        {
+            var isInside = TopLeftPoint.PointX <= point.PointX
+                           && LowerRightPoint.PointX >= point.PointX
+                           && TopLeftPoint.PointY <= point.PointY
+                           && LowerRightPoint.PointY >= point.PointY;
+            return isInside;
+        }
+    }
+}
