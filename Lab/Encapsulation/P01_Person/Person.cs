d873bd13a2f102baadee2919319581f6bfb0d360
Gyunay Daud <prohause@live.com>
2018-10-24 16:51:04 +0300

    Problem 01 solved

---
+++ Lab/Encapsulation/P01_Person/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public string FirstName
+        {
+            get => firstName;
+            set => firstName = value;
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set => lastName = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
