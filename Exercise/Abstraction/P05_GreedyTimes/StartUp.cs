44ce411060a6e50c25b82aefb1477d1d41acd696
Gyunay Daud <prohause@live.com>
2018-10-24 16:32:59 +0300

    Problem 06 refactored 90/100

---
+++ Exercise/Abstraction/P05_GreedyTimes/StartUp.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_GreedyTimes
+{
+    public class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            long bagCapacity = long.Parse(Console.ReadLine());
+            string[] content = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var bag = new Dictionary<string, Dictionary<string, long>>();
+
+            for (int i = 0; i < content.Length; i += 2)
+            {
+                string currentType = content[i];
+                long amount = long.Parse(content[i + 1]);
+
+                string type = string.Empty;
+
+                if (currentType.Length == 3)
+                {
+                    type = "Cash";
+                }
+                else if (currentType.ToLower().EndsWith("gem"))
+                {
+                    type = "Gem";
+                }
+                else if (currentType.ToLower() == "gold")
+                {
+                    type = "Gold";
+                }
+
+                if (type == "" || bagCapacity < bag.Values.Select(x => x.Values.Sum()).Sum() + amount)
+                {
+                    continue;
+                }
+
+                switch (type)
+                {
+                    case "Gem":
+                        if (!bag.ContainsKey(type))
+                        {
+                            if (bag.ContainsKey("Gold"))
+                            {
+                                if (amount > bag["Gold"].Values.Sum())
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (bag[type].Values.Sum() + amount > bag["Gold"].Values.Sum())
+                        {
+                            continue;
+                        }
+                        break;
+
+                    case "Cash":
+                        if (!bag.ContainsKey(type))
+                        {
+                            if (bag.ContainsKey("Gem"))
+                            {
+                                if (amount > bag["Gem"].Values.Sum())
+                                {
+                                    continue;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (bag[type].Values.Sum() + amount > bag["Gem"].Values.Sum())
+                        {
+                            continue;
+                        }
+                        break;
+                }
+
+                if (!bag.ContainsKey(type))
+                {
+                    bag[type] = new Dictionary<string, long>();
+                }
+
+                if (!bag[type].ContainsKey(currentType))
+                {
+                    bag[type][currentType] = 0;
+                }
+
+                bag[type][currentType] += amount;
+            }
+
+            foreach (var x in bag)
+            {
+                Console.WriteLine($"<{x.Key}> ${x.Value.Values.Sum()}");
+                foreach (var item2 in x.Value.OrderByDescending(y => y.Key).ThenBy(y => y.Value))
+                {
+                    Console.WriteLine($"##{item2.Key} - {item2.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Exercise/Abstraction/P06_Sneaking/Sneaking.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+namespace P06_Sneaking
+{
+    internal class Sneaking
+    {
+        private static char[][] _room;
+
+        private static void Main()
+        {
+            int rowCount = int.Parse(Console.ReadLine());
+            _room = new char[rowCount][];
+
+            for (int row = 0; row < rowCount; row++)
+            {
+                var input = Console.ReadLine().ToCharArray();
+                _room[row] = new char[input.Length];
+                for (int col = 0; col < input.Length; col++)
+                {
+                    _room[row][col] = input[col];
+                }
+            }
+
+            var moves = Console.ReadLine().ToCharArray();
+            int[] samPosition = new int[2];
+            for (int row = 0; row < _room.Length; row++)
+            {
+                for (int col = 0; col < _room[row].Length; col++)
+                {
+                    if (_room[row][col] == 'S')
+                    {
+                        samPosition[0] = row;
+                        samPosition[1] = col;
+                    }
+                }
+            }
+            foreach (var t in moves)
+            {
+                for (int row = 0; row < _room.Length; row++)
+                {
+                    for (int col = 0; col < _room[row].Length; col++)
+                    {
+                        switch (_room[row][col])
+                        {
+                            case 'b' when row >= 0 && row < _room.Length && col + 1 >= 0 && col + 1 < _room[row].Length:
+                                _room[row][col] = '.';
+                                _room[row][col + 1] = 'b';
+                                col++;
+                                break;
+
+                            case 'b':
+                                _room[row][col] = 'd';
+                                break;
+
+                            case 'd' when row >= 0 && row < _room.Length && col - 1 >= 0 && col - 1 < _room[row].Length:
+                                _room[row][col] = '.';
+                                _room[row][col - 1] = 'd';
+                                break;
+
+                            case 'd':
+                                _room[row][col] = 'b';
+                                break;
+
+                            default:
+                                break;
+                        }
+                    }
+                }
+
+                int[] getEnemy = new int[2];
+                for (int j = 0; j < _room[samPosition[0]].Length; j++)
+                {
+                    if (_room[samPosition[0]][j] == '.' || _room[samPosition[0]][j] == 'S')
+                    {
+                        continue;
+                    }
+
+                    getEnemy[0] = samPosition[0];
+                    getEnemy[1] = j;
+                }
+                if (samPosition[1] < getEnemy[1] && _room[getEnemy[0]][getEnemy[1]] == 'd' && getEnemy[0] == samPosition[0])
+                {
+                    _room[samPosition[0]][samPosition[1]] = 'X';
+                    Console.WriteLine($"Sam died at {samPosition[0]}, {samPosition[1]}");
+                    Print();
+                }
+                else if (getEnemy[1] < samPosition[1] && _room[getEnemy[0]][getEnemy[1]] == 'b' && getEnemy[0] == samPosition[0])
+                {
+                    _room[samPosition[0]][samPosition[1]] = 'X';
+                    Console.WriteLine($"Sam died at {samPosition[0]}, {samPosition[1]}");
+                    Print();
+                }
+
+                _room[samPosition[0]][samPosition[1]] = '.';
+                switch (t)
+                {
+                    case 'U':
+                        samPosition[0]--;
+                        break;
+
+                    case 'D':
+                        samPosition[0]++;
+                        break;
+
+                    case 'L':
+                        samPosition[1]--;
+                        break;
+
+                    case 'R':
+                        samPosition[1]++;
+                        break;
+
+                    default:
+                        break;
+                }
+                _room[samPosition[0]][samPosition[1]] = 'S';
+
+                for (int j = 0; j < _room[samPosition[0]].Length; j++)
+                {
+                    if (_room[samPosition[0]][j] == '.' || _room[samPosition[0]][j] == 'S')
+                    {
+                        continue;
+                    }
+
+                    getEnemy[0] = samPosition[0];
+                    getEnemy[1] = j;
+                }
+
+                if (_room[getEnemy[0]][getEnemy[1]] != 'N' || samPosition[0] != getEnemy[0])
+                {
+                    continue;
+                }
+
+                {
+                    _room[getEnemy[0]][getEnemy[1]] = 'X';
+                    Console.WriteLine("Nikoladze killed!");
+                    Print();
+                }
+            }
+        }
+
+        private static void Print()
+        {
+            foreach (var t1 in _room)
+            {
+                foreach (var t2 in t1)
+                {
+                    Console.Write(t2);
+                }
+
+                Console.WriteLine();
+            }
+            Environment.Exit(0);
+        }
+    }
+}
