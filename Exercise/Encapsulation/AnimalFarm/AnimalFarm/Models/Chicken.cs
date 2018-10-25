147058d409b76e0dedf18a126d9431d318ad4f13
Gyunay Daud <prohause@live.com>
2018-10-25 16:33:58 +0300

    Problem 03 solved

--- Exercise/Encapsulation/AnimalFarm/AnimalFarm/Models/Chicken.cs
+++ Exercise/Encapsulation/AnimalFarm/AnimalFarm/Models/Chicken.cs
@@ -1,72 +1,73 @@
-﻿namespace AnimalFarm.Models
+﻿using System;
+
+namespace AnimalFarm.Models
 {
     public class Chicken
     {
-        public const int MinAge = 0;
-        public const int MaxAge = 15;
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
 
-        protected string name;
-        internal int age;
+        private string _name;
+        private int _age;
 
-        internal Chicken(string name, int age)
+        public Chicken(string name, int age)
         {
-            this.name = name;
-            this.age = age;
+            Name = name;
+            Age = age;
         }
 
         public string Name
         {
-            get
-            {
-                return this.name;
-            }
+            get => _name;
 
-            internal set
+            private set
             {
-                this.name = value;
+                if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty.");
+                }
+                _name = value;
             }
         }
 
         public int Age
         {
-            get
-            {
-                return this.age;
-            }
+            get => _age;
 
-            protected set
+            private set
             {
-                this.age = value;
+                if (value < MinAge || MaxAge < value)
+                {
+                    throw new ArgumentException($"Age should be between {MinAge} and {MaxAge}.");
+                }
+                _age = value;
             }
         }
 
-        public double ProductPerDay
-        {
-			get
-			{				
-				return this.CalculateProductPerDay();
-			}
-        }
+        public double ProductPerDay => CalculateProductPerDay();
 
-        public double CalculateProductPerDay()
+        private double CalculateProductPerDay()
         {
-            switch (this.Age)
+            switch (Age)
             {
                 case 0:
                 case 1:
                 case 2:
                 case 3:
                     return 1.5;
+
                 case 4:
                 case 5:
                 case 6:
                 case 7:
                     return 2;
+
                 case 8:
                 case 9:
                 case 10:
                 case 11:
                     return 1;
+
                 default:
                     return 0.75;
             }
