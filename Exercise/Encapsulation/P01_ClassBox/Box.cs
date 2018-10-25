dd980813e78d8e68b5bf8134c70726b2c22d2eb2
Gyunay Daud <prohause@live.com>
2018-10-25 16:03:42 +0300

    Problem 01 solved

---
+++ Exercise/Encapsulation/P01_ClassBox/Box.cs
@@ -0,0 +1,49 @@
+﻿namespace P01_ClassBox
+{
+    public class Box
+    {
+        public double Length
+        {
+            get;
+        }
+
+        public double Width
+        {
+            get;
+        }
+
+        public double Height
+        {
+            get;
+        }
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double CalculateSurfaceArea()
+        {
+            return 2 * Length * Width + 2 * Width * Height + 2 * Length * Height;
+        }
+
+        public override string ToString()
+        {
+            return $"Surface Area - {CalculateSurfaceArea():F2}" +
+                   $"\nLateral Surface Area - {CalculateLateralArea():F2}" +
+                   $"\nVolume - {CalculateVolume():F2}";
+        }
+
+        private double CalculateVolume()
+        {
+            return Length * Width * Height;
+        }
+
+        private double CalculateLateralArea()
+        {
+            return (2 * Length + 2 * Width) * Height;
+        }
+    }
+}
---
+++ Exercise/Encapsulation/P01_ClassBox/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P01_ClassBox
+{
+    public class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            var myBox = new Box(length, width, height);
+            Console.WriteLine(myBox);
+        }
+    }
+}
