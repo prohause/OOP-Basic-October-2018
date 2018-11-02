598349b757d1a185ab08ffeb4b35fda3dd919a6d
Gyunay Daud <prohause@live.com>
2018-11-02 17:15:47 +0200

    Initial commit

---
+++ Exercise/Inheritance/P04_Online_Radio_Database/Models/InvalidSongException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace P04_Online_Radio_Database.Models
+{
+    internal class InvalidSongException : ArgumentException
+    {
+        public InvalidSongException() : base("Invalid song.")
+        {
+        }
+    }
+}
---
+++ Exercise/Inheritance/P04_Online_Radio_Database/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using P04_Online_Radio_Database.Models;
+using System;
+
+namespace P04_Online_Radio_Database
+{
+    public class StartUp
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                throw new InvalidSongException();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
